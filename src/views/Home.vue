33f0f0aeeaa9f2b2a2daf8905e6e69ccd190ffce
Ish Sookun <ishwon@opensuse.org>
2021-01-05 13:43:57 +0400

    Removed agenda component. Added a simple page for the agenda button in the header.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <IntroAnimationComponent />
     <TextComponent />
-    <ScheduleComponent />
+    <!-- <ScheduleComponent /> -->
     <SponsorsComponent />
   </div>
 </template>
@@ -10,7 +10,7 @@
 <script>
 // import IntroComponent from "@/components/home/IntroComponent";
 import IntroAnimationComponent from "@/components/home/IntroAnimationComponent";
-import ScheduleComponent from "@/components/home/ScheduleComponent";
+// import ScheduleComponent from "@/components/home/ScheduleComponent";
 import SponsorsComponent from "@/components/home/SponsorsComponent";
 // import SponsorsComponent from "@/components/home/SponsorsComponentConf";
 // import SpeakersComponent from "@/components/home/SpeakersComponent";
@@ -24,7 +24,7 @@ export default {
     IntroAnimationComponent,
     SponsorsComponent,
     TextComponent,
-    ScheduleComponent,
+    // ScheduleComponent,
     // RegisterComponent
     // SpeakersComponent
   }
