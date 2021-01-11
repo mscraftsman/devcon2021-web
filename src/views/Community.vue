4bc569ad8af9bc0a8c6aa087daa3854d3639f80b
Ish Sookun <ishwon@opensuse.org>
2021-01-11 19:34:07 +0400

    Update Communites & Extras

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -1,33 +1,55 @@
 <template>
   <div class="container__fw page">
-    <h1>Community</h1>
+    <h1>Networking & Community</h1>
     <div class="content">
       <p>
-        All attendees, speakers, sponsors and volunteers at our conference are
-        required to agree with the following code of conduct. Organisers will
-        enforce this code throughout the event. We expect cooperation from all
-        participants to help ensure a safe environment for everybody.
+        Take the chance to exchange with other attendees, speakers and 
+        partners, have some interesting conversations about software development, 
+        latest information gained during one of the presentations and let others
+        know about your ups & downs in your projects. 
       </p>
 
-      <h2>Mauritius Software Craftsmanship Community</h2>
-      <img
-        src="https://www.mscc.mu/content/images/2018/12/be-excellent-to-each-other.jpg"
-        alt="Be excellent to each other"
-      />
-      <p>You have our contact details in the emails we've sent.</p>
+      <p>
+        Our Partners are here for existing & new talents for either recruitment 
+        or internship or simply for a chit chat. Make the most out of it! Our 
+        Happy Hour is just the right moment to maybe make a new friend, seal 
+        some deals in a more relaxed atmosphere. Think about it. 
+      </p>
+
+      <p>
+        But above all, come and enjoy this vibrant atmosphere full of technology 
+        & activities! 
+      </p>
+
+      <h2>Google Developers Group Mauritius</h2>
+      <p>
+        The GDG Mauritius is a local community-ran meetup for developers 
+        interested in resources and technology from Google Developers. 
+      </p>
+
+      <p>
+        The MSCC has an annual event called the Developers Conference which 
+        is free of charge community-driven, IT tech conference hosted annually 
+        in Mauritius. Since 2016 there is an extra venue at the Flying Dodo. 
+      </p>
+
+      <p>
+        GDGs are local groups of developers who are specifically interested 
+        in Google products and APIs. Each local group is called a GDG chapter 
+        and can host a variety of technical activities for developers - from 
+        just a few people getting together to watch our latest videos, to 
+        large gatherings with demos and tech talks, to hackathons. 
+      </p>
+
+      <p>
+        Disclaimer: GDG Mauritius is an independent user group; our activities 
+        and the opinions expressed here should in no way be linked to Google, 
+        the corporation. 
+      </p>
 
       <h2>Front-end Coders - Mauritius</h2>
       <p>
-        Our conference is dedicated to providing a harassment-free conference
-        experience for everyone, regardless of gender, gender identity and
-        expression, age, sexual orientation, disability, physical appearance,
-        body size, race, ethnicity, religion (or lack thereof), or technology
-        choices. We do not tolerate harassment of conference participants in any
-        form. Sexual language and imagery is not appropriate for any conference
-        venue, including talks, workshops, parties, Twitter and other online
-        media. Conference participants violating these rules may be sanctioned
-        or expelled from the conference at the discretion of the conference
-        organisers.
+        To be completed.
       </p>
 
       <h2>Linux User Group of Mauritius</h2>
@@ -38,6 +60,29 @@
         organising Linux installation festivals and helping users with common *nix 
         troubleshooting. LUGM members often speak at local events about open source 
         software.
+      </p>
+
+      <h2>Mauritius Maker Community</h2>
+      <p>
+         The Mauritius Makers community is a group of enthusiasts and passionate 
+         Makers, Hackers, Artists, Hardware Developers, Inventors, Designers 
+         and Tinkerers geared primarily toward technological innovation such as 
+         Electronics, Open Source Hardware, Internet Of Things, Robotics, 3D 
+         Printing, Embedded Software, Hardware, Arduino, Small Board Computers, 
+         SBC like the Raspbery PI, Printed Circuit Board design, DIY, Wood Work, 
+         Metal Work, Computer Aided Designing, Wearables(can be extended to Cosplayers). 
+      </p>
+
+      <p>
+         The MSCC has an annual event called the Developers Conference which is 
+         free of charge community-driven, IT tech conference hosted annually 
+         in Mauritius. Since 2016 there is an extra venue at the Flying Dodo. 
+      </p>
+
+      <p>
+         The MMC is organising several activities: Several sessions with one or 
+         multiple speakers covering a wide range of topics, hands on practicals 
+         on 3D-printing, Demo of SBC, IoT Demo, Home Automation, plenty of Goodies.
       </p>
 
       <h2>openSUSE Mauritius</h2>
@@ -55,6 +100,26 @@
         The Ubuntu LoCo team is undergoing a revival after being dormant for some
         years. Ubuntu users are welcomed to <a href="https://loco.ubuntu.com/teams/ubuntu-mu/">
         join the group</a> on the Ubuntu loco team portal.
+      </p>
+
+      <h2>Python Mauritius User Group (pymug)</h2>
+      <p>
+         The Python Mauritius User Group (pymug) is the local Python user group 
+         of Mauritius. Our aim is to provide opportunities to everybody to upskill 
+         their level of Python and to ease industry-adoption of Python-related 
+         technologies. A nice overview of the User Group can be found on the MSCC 
+         website here. To sum it up, we care about Python education. 
+         Our website: <a href="https://www.pymug.com">pymug.com</a>
+      </p>
+
+      <p>
+         We'll be present at the DevCon which is an amazing community-driven event. 
+      </p>
+
+      <p>
+         The Pymug organises year-round events. Topic days, meetups, from scratch 
+         sessions as well as University topic coverage. Have a look at our 2019 
+         report. 
       </p>
     </div>
   </div>
