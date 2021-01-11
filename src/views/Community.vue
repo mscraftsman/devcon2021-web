5cd5b3b3ce9736745f6aa23239d1732900e27248
Ish Sookun <ishwon@opensuse.org>
2021-01-11 12:27:00 +0400

    Removed code-of-conduct text from Community page

--- src/views/Community.vue
+++ src/views/Community.vue
@@ -56,60 +56,6 @@
         years. Ubuntu users are welcomed to <a href="https://loco.ubuntu.com/teams/ubuntu-mu/">
         join the group</a> on the Ubuntu loco team portal.
       </p>
-
-      <h2>The Less Quick Version</h2>
-      <p>
-        Harassment includes offensive verbal comments related to gender, gender
-        identity and expression, age, sexual orientation, disability, physical
-        appearance, body size, race, ethnicity, religion, technology choices,
-        sexual images in public spaces, deliberate intimidation, stalking,
-        following, harassing photography or recording, sustained disruption of
-        talks or other events, inappropriate physical contact, and unwelcome
-        sexual attention.
-      </p>
-      <p>
-        Participants asked to stop any harassing behavior are expected to comply
-        immediately.
-      </p>
-      <p>
-        Sponsors are also subject to the anti-harassment policy. In particular,
-        sponsors should not use sexualized images, activities, or other
-        material. Booth staff (including volunteers) should not use sexualized
-        clothing/uniforms/costumes, or otherwise create a sexualized
-        environment.
-      </p>
-      <p>
-        If a participant engages in harassing behavior, the conference
-        organisers may take any action they deem appropriate, including warning
-        the offender or expulsion from the conference with no refund.
-      </p>
-      <p>
-        If you are being harassed, notice that someone else is being harassed,
-        or have any other concerns, please contact a member of conference staff
-        immediately. Conference staff can be identified as they'll be wearing
-        branded t-shirts.
-      </p>
-      <p>
-        Conference staff will be happy to help participants contact hotel/venue
-        security or local law enforcement, provide escorts, or otherwise assist
-        those experiencing harassment to feel safe for the duration of the
-        conference. We value your attendance.
-      </p>
-      <p>
-        We expect participants to follow these rules at conference and workshop
-        venues and conference-related social events
-      </p>
-      <p>
-        Original source and credit:
-        <a href="http://2012.jsconf.us/#/about"
-          >http://2012.jsconf.us/#/about</a
-        >
-        &
-        <a
-          href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy"
-          >The Ada Initiative</a
-        >
-      </p>
     </div>
   </div>
 </template>
